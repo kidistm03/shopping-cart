dd67e33a01af228e1826d2bda1d086c2d144bb96
kidistm03 <kidistmeseret1@gmail.com>
2026-08-29 18:17:30 +0300

    build product component which is product card

---
+++ src/components/product/ProductCard.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import "./ProductCard.css";
+
+function ProductCard({ product }) {
+  return (
+    <div className="product-card">
+      <img
+        src={product.image}
+        alt={product.title}
+        className="product-image"
+      />
+
+      <div className="product-info">
+        <h2>{product.title}</h2>
+
+        <p className="product-price">
+          ${product.price}
+        </p>
+
+        <p>⭐ {product.rating.rate}</p>
+
+        <Link to={`/shop/${product.id}`} className="view-button">
+          View Product
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
