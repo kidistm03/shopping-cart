2b0bb0d29fd70d042590535a5c882e4a7e69b9f8
kidistm03 <kidistmeseret1@gmail.com>
2026-08-29 15:49:56 +0300

    create footer and its css

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2026 MyShop. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
