8a8f6a75469763785a3db4d2c8b068fad190096c
kidistm03 <kidistmeseret1@gmail.com>
2026-08-29 15:37:13 +0300

    create product detail page

---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,10 @@
+function ProductDetail() {
+  return (
+    <div>
+      <h1>Product Detail</h1>
+      <p>Product information will appear here.</p>
+    </div>
+  );
+}
+
+export default ProductDetail;
