24c5430a1bc8c5f5dcd48ad101ca3b344a504375
kidistm03 <kidistmeseret1@gmail.com>
2026-08-29 15:40:56 +0300

    update app.jsx import all the pages and react route dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import ProductDetail from "./pages/ProductDetail";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <Home />
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      <Route path="/shop" element={<Shop />} />
+
+      <Route path="/shop/:id" element={<ProductDetail />} />
+
+      <Route path="/cart" element={<Cart />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
