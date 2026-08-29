3f5e2b93c90ef22c082fb55f115d05d22d3240e9
kidistm03 <kidistmeseret1@gmail.com>
2026-08-29 18:19:38 +0300

    build product component which is product grid

---
+++ src/components/product/ProductGrid.jsx
@@ -0,0 +1,17 @@
+import ProductCard from "./ProductCard";
+import "./ProductGrid.css";
+
+function ProductGrid({ products }) {
+  return (
+    <div className="product-grid">
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          product={product}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default ProductGrid;
