c178414912a6a581c6ac8842915ec6b0b2be5db3
kidistm03 <kidistmeseret1@gmail.com>
2026-08-29 18:20:41 +0300

    update shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,41 @@
+import useProducts from "../hooks/useProducts";
+import ProductGrid from "../components/product/ProductGrid";
+
 function Shop() {
+  const {
+    products,
+    loading,
+    error,
+    retry
+  } = useProducts();
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Shop</h1>
+        <p>Loading products...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <h1>Shop</h1>
+        <p>{error}</p>
+
+        <button onClick={retry}>
+          Try Again
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Shop</h1>
-      <p>All our products will appear here.</p>
+
+      <ProductGrid products={products} />
     </div>
   );
 }
