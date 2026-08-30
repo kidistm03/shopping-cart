c1c783e3704d45c06fffb824017595e34a7ce7a3
kidistm03 <kidistmeseret1@gmail.com>
2026-08-30 18:30:23 +0300

    add search bar

---
+++ src/components/shop/SearchBar.jsx
@@ -0,0 +1,16 @@
+import "./SearchBar.css";
+
+function SearchBar({ search, onSearch }) {
+  return (
+    <div className="search-box">
+      <input
+        type="text"
+        placeholder="Search products..."
+        value={search}
+        onChange={(event) => onSearch(event.target.value)}
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
