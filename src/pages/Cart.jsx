3e1900065c0fcd5417af9bd5bee9cf4405515ccc
kidistm03 <kidistmeseret1@gmail.com>
2026-08-30 22:57:09 +0300

    update cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,55 @@
 import useCart from "../hooks/useCart";
 
+import CartItem from "../components/cart/CartItem";
+import OrderSummary from "../components/cart/OrderSummary";
+import EmptyCart from "../components/cart/EmptyCart";
+
+import "../styles/Cart.css";
+
 function Cart() {
-  const { cart } = useCart();
+  const { cart, dispatch } = useCart();
+
+  function clearCart() {
+    const confirmed = window.confirm(
+      "Are you sure you want to clear your cart?"
+    );
+
+    if (confirmed) {
+      dispatch({
+        type: "CLEAR_CART"
+      });
+    }
+  }
+
+  if (cart.length === 0) {
+    return <EmptyCart />;
+  }
 
   return (
-    <div>
-      <h1>Shopping Cart</h1>
+    <div className="cart-page">
+      <div className="cart-header">
+        <h1>Shopping Cart</h1>
 
-      <p>Items in cart: {cart.length}</p>
+        <button
+          className="clear-cart-button"
+          onClick={clearCart}
+        >
+          Clear Cart
+        </button>
+      </div>
+
+      <div className="cart-content">
+        <div className="cart-items">
+          {cart.map((item) => (
+            <CartItem
+              key={item.id}
+              item={item}
+            />
+          ))}
+        </div>
+
+        <OrderSummary cart={cart} />
+      </div>
     </div>
   );
 }
