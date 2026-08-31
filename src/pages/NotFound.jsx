1b9fc8a31eb8184d896f42702967054940c7c516
kidistm03 <kidistmeseret1@gmail.com>
2026-08-31 18:55:52 +0300

    make the css part

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,22 @@
+import { Link } from "react-router-dom";
+
+import "../styles/NotFound.css";
+
 function NotFound() {
   return (
-    <div>
+    <main className="not-found">
       <h1>404</h1>
-      <p>Page not found.</p>
-    </div>
+
+      <h2>Page Not Found</h2>
+
+      <p>
+        Sorry, the page you are looking for does not exist.
+      </p>
+
+      <Link to="/">
+        Go Home
+      </Link>
+    </main>
   );
 }
 
